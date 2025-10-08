8dbe9530c379c0970dc7bc88819c7a0b839a9f13
Faizan Shaikh <145644859+faizan7745@users.noreply.github.com>
2025-10-08 19:09:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, faizan shaikh!'
+    return 'Hello, faizan shaikh!how are you..'
 
 if __name__ == '__main__':
     app.run()
